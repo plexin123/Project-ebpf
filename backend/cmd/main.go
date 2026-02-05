1d2075d4f054ca20f73c50b84c027c360a982858
Paul Puma <puma@Pauls-MacBook-Pro.local>
2026-02-04 16:00:19 -0800

    Fix capture data, for packet2, printed

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -6,8 +6,10 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"net"
+	"strings"
 	"time"
 )
 
@@ -15,10 +17,18 @@ import (
 // to achive first goal
 
 type TCP_Packet struct {
-	service_origin      [32]byte
-	service_destination [32]byte
-	portOrigin          uint32
-	portDestination     uint32
+	// there are not array of characters just bytes supported in golang
+	Service_origin      [32]byte
+	Service_destination [32]byte
+	PortOrigin          uint32
+	PortDestination     uint32
+}
+
+type TCP_Packet2 struct {
+	Service_origin      string
+	Service_destination string
+	PortOrigin          uint32
+	PortDestination     uint32
 }
 
 func main() {
@@ -44,10 +54,34 @@ func main() {
 		var packet TCP_Packet
 		raw_bytes := bytes.NewReader(buffer_of_bytes[:n])
 		// fmt.Printf("this is the buffer of bytes", buffer_of_bytes)
-		fmt.Printf("Raw bytes in hex: %x\n", raw_bytes)
-		fmt.Printf("data", raw_bytes)
-		err = binary.Read(raw_bytes, binary.LittleEndian, packet)
-		fmt.Printf("asdasdasd", packet)
+		// fmt.Printf("Raw bytes in hex: %x\n", raw_bytes)
+		// fmt.Printf("data", raw_bytes)
+		// read raw bytes
+		err = binary.Read(raw_bytes, binary.LittleEndian, &packet)
+		var packet2show TCP_Packet2
+		destination := string(packet.Service_destination[:])
+		origin := string(packet.Service_origin[:])
+		packet2show.Service_origin = strings.ReplaceAll(origin, "\u0000", "")
+		packet2show.Service_destination = strings.ReplaceAll(destination, "\u0000", "")
+		packet2show.PortDestination = packet.PortDestination
+		packet2show.PortOrigin = packet.PortOrigin
+
+		ans, err := json.Marshal(packet2show)
+		if err != nil {
+			break
+		}
+		fmt.Println(string(ans))
+
+		// fmt.Print(packet2show)
+
+		// fmt.Println(origin, destination)
+		// fmt.Println(packet.Service_destination.decode('utf-8'))
+		// jsonData, err := json.Marshal(packet)
+		// if err != nil {
+		// 	fmt.Printf("this is an error", err)
+		// }
+		// jsonString := string(jsonData)
+		// fmt.Printf(jsonString)
 		// fmt.Printf("Puertos: %d -> %d\n", packet.portOrigin, packet.portDestination)
 
 		fmt.Printf("It is working")
