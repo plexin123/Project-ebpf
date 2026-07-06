c0ea2bec9364b62004286f26293a4f04d531d47c
Paul Puma <archimago05@hotmail.com>
2026-07-05 22:19:56 -0700

    Completed go collector

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -6,88 +6,82 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
-	"encoding/json"
 	"fmt"
-	"net"
-	"strings"
-	"time"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/ringbuf"
 )
 
 // we need to create a udp server to listen for incoming packets parse it and then print that
 // to achive first goal
 
-type TCP_Packet struct {
-	// there are not array of characters just bytes supported in golang
-	Service_origin      [32]byte
-	Service_destination [32]byte
-	PortOrigin          uint32
-	PortDestination     uint32
-}
-
-// this a sample
-type TCP_Packet2 struct {
-	Service_origin      string
-	Service_destination string
-	PortOrigin          uint32
-	PortDestination     uint32
+type ProcessEvent struct {
+	PID  uint32
+	PPID uint32
+	Comm [16]byte
 }
 
 func main() {
-	addr, err := net.ResolveUDPAddr("udp", ":8083")
-	if err != nil {
-		panic(err)
-	}
-	conn, err := net.ListenUDP("udp", addr)
-	if err != nil {
-		panic(err)
-	}
-	defer conn.Close()
-	for {
-		fmt.Printf("first step")
-		// create a buffer to store the bytes
-		buffer_of_bytes := make([]byte, 72)
-		// read the bytes
-		n, _, err := conn.ReadFromUDP(buffer_of_bytes)
-		if err != nil {
-			fmt.Println("Error from the reading the socket", err)
-			continue
-		}
-		var packet TCP_Packet
-		raw_bytes := bytes.NewReader(buffer_of_bytes[:n])
-		// fmt.Printf("this is the buffer of bytes", buffer_of_bytes)
-		// fmt.Printf("Raw bytes in hex: %x\n", raw_bytes)
-		// fmt.Printf("data", raw_bytes)
-		// read raw bytes
-		err = binary.Read(raw_bytes, binary.LittleEndian, &packet)
-		var packet2show TCP_Packet2
-		destination := string(packet.Service_destination[:])
-		origin := string(packet.Service_origin[:])
-		packet2show.Service_origin = strings.ReplaceAll(origin, "\u0000", "")
-		packet2show.Service_destination = strings.ReplaceAll(destination, "\u0000", "")
-		packet2show.PortDestination = packet.PortDestination
-		packet2show.PortOrigin = packet.PortOrigin
 
-		ans, err := json.Marshal(packet2show)
+	spec, err := ebpf.LoadCollectionSpec("monitor.bpf.o")
+
+	if err != nil {
+		log.Fatalf("failed to load eBPF spec: %v", err)
+	}
+
+	coll, err := ebpf.NewCollection(spec)
+
+	if err != nil {
+		log.Fatalf("failed to create collection %v", err)
+	}
+
+	defer coll.Close()
+
+	tp, err := link.Tracepoint("syscalls", "sys_enter_execve", coll.Programs["new_program"], nil)
+
+	if err != nil {
+		log.Fatalf("failed to attach tracepoint %v", err)
+	}
+
+	defer tp.Close()
+
+	reader, err := ringbuf.NewReader(coll.Maps["events"])
+
+	if err != nil {
+		log.Fatalf("failed to create a new reader %v", err)
+	}
+
+	defer reader.Close()
+
+	sig := make(chan os.Signal, 1)
+
+	signal.Notify(sig, os.Interrupt)
+
+	go func() {
+		<-sig
+		reader.Close()
+	}()
+
+	for {
+		record, err := reader.Read()
+
 		if err != nil {
 			break
 		}
-		fmt.Println(string(ans))
 
-		// fmt.Print(packet2show)
+		var event ProcessEvent
 
-		// fmt.Println(origin, destination)
-		// fmt.Println(packet.Service_destination.decode('utf-8'))
-		// jsonData, err := json.Marshal(packet)
-		// if err != nil {
-		// 	fmt.Printf("this is an error", err)
-		// }
-		// jsonString := string(jsonData)
-		// fmt.Printf(jsonString)
-		// fmt.Printf("Puertos: %d -> %d\n", packet.portOrigin, packet.portDestination)
+		if err := binary.Read(bytes.NewReader(record.RawSample), binary.LittleEndian, &event); err != nil {
+			continue
+		}
+		name := string(bytes.TrimRight(event.Comm[:], "\x00"))
 
-		fmt.Printf("It is working")
+		fmt.Printf("pid: %-6d  ppid: %-6d  comm: %s\n", event.PID, event.PPID, name)
 
-		time.Sleep(3 * time.Second)
 	}
 
 }
