fd790ab6c1c038191b803c5ee656fa8e408d6d4b
Paul Puma <archimago05@hotmail.com>
2026-07-17 12:20:41 -0700

    Adding profiler WIP

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -14,45 +14,138 @@ import (
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
 	"github.com/cilium/ebpf/ringbuf"
+	"github.com/cilium/ebpf/rlimit"
 )
 
-// we need to create a udp server to listen for incoming packets parse it and then print that
-// to achive first goal
+// Latency time for each functionality
+// Keep it 1 function
+// compile one test golang file
+// run it against the lumentrace -> see function how long does it take
 
-type ProcessEvent struct {
-	PID  uint32
-	PPID uint32
-	Comm [16]byte
+// type ProcessEvent struct {
+// 	PID  uint32
+// 	PPID uint32
+// 	Comm [16]byte
+// }
+
+// func main() {
+
+// 	spec, err := ebpf.LoadCollectionSpec("monitor.bpf.o")
+
+// 	if err != nil {
+// 		log.Fatalf("failed to load eBPF spec: %v", err)
+// 	}
+
+// 	coll, err := ebpf.NewCollection(spec)
+
+// 	if err != nil {
+// 		log.Fatalf("failed to create collection %v", err)
+// 	}
+
+// 	defer coll.Close()
+
+// 	tp, err := link.Tracepoint("syscalls", "sys_enter_execve", coll.Programs["new_program"], nil)
+
+// 	if err != nil {
+// 		log.Fatalf("failed to attach tracepoint %v", err)
+// 	}
+
+// 	defer tp.Close()
+
+// 	reader, err := ringbuf.NewReader(coll.Maps["events"])
+
+// 	if err != nil {
+// 		log.Fatalf("failed to create a new reader %v", err)
+// 	}
+
+// 	defer reader.Close()
+
+// 	sig := make(chan os.Signal, 1)
+
+// 	signal.Notify(sig, os.Interrupt)
+
+// 	go func() {
+// 		<-sig
+// 		reader.Close()
+// 	}()
+
+// 	for {
+// 		record, err := reader.Read()
+
+// 		if err != nil {
+// 			break
+// 		}
+
+// 		var event ProcessEvent
+
+// 		if err := binary.Read(bytes.NewReader(record.RawSample), binary.LittleEndian, &event); err != nil {
+// 			continue
+// 		}
+// 		name := string(bytes.TrimRight(event.Comm[:], "\x00"))
+
+// 		fmt.Printf("pid: %-6d  ppid: %-6d  comm: %s\n", event.PID, event.PPID, name)
+
+// 	}
+
+// }
+
+type Latency_event struct {
+	DurationNS uint64
+	PID        uint32
+	Comm       [16]byte
 }
 
 func main() {
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Fatalf("failed to remove memlock: %v", err)
+	}
+	if len(os.Args) < 3 {
+		log.Fatalf("usage: profiler <binary> <function>")
+	}
+	binaryPath := os.Args[1]
+	funcName := os.Args[2]
 
-	spec, err := ebpf.LoadCollectionSpec("monitor.bpf.o")
-
+	spec, err := ebpf.LoadCollectionSpec("profiler.bpf.o")
 	if err != nil {
-		log.Fatalf("failed to load eBPF spec: %v", err)
+		log.Fatalf("failed to load spec: %v", err)
 	}
 
 	coll, err := ebpf.NewCollection(spec)
-
 	if err != nil {
-		log.Fatalf("failed to create collection %v", err)
+		log.Fatalf("failed to create collection: %v", err)
 	}
-
 	defer coll.Close()
 
-	tp, err := link.Tracepoint("syscalls", "sys_enter_execve", coll.Programs["new_program"], nil)
+	// open binary
+
+	ex, err := link.OpenExecutable(binaryPath)
 
 	if err != nil {
-		log.Fatalf("failed to attach tracepoint %v", err)
+		log.Fatalf("failed to open binary: %v", err)
 	}
 
-	defer tp.Close()
+	// attach entry probe
 
-	reader, err := ringbuf.NewReader(coll.Maps["events"])
+	up, err := ex.Uprobe(funcName, coll.Programs["trace_enter"], nil)
 
 	if err != nil {
-		log.Fatalf("failed to create a new reader %v", err)
+		log.Fatalf("failed to attached uprobe: %v", err)
+	}
+
+	defer up.Close()
+
+	// exit probe
+	ret, err := ex.Uretprobe(funcName, coll.Programs["trace_exit"], nil)
+
+	if err != nil {
+		log.Fatalf("Failed to attach uretprobe: %v", err)
+	}
+	defer ret.Close()
+
+	reader, err := ringbuf.NewReader(coll.Maps["latency_events"])
+
+	if err != nil {
+		log.Fatalf("failed to open ring buffer: %v", err)
 	}
 
 	defer reader.Close()
@@ -68,20 +161,22 @@ func main() {
 
 	for {
 		record, err := reader.Read()
-
 		if err != nil {
 			break
 		}
-
-		var event ProcessEvent
-
-		if err := binary.Read(bytes.NewReader(record.RawSample), binary.LittleEndian, &event); err != nil {
+		var event Latency_event
+		if err := binary.Read(
+			bytes.NewReader(record.RawSample),
+			binary.LittleEndian,
+			&event,
+		); err != nil {
+			log.Printf("Failed to parse event: %v", err)
 			continue
 		}
 		name := string(bytes.TrimRight(event.Comm[:], "\x00"))
-
-		fmt.Printf("pid: %-6d  ppid: %-6d  comm: %s\n", event.PID, event.PPID, name)
-
+		fmt.Printf("pid: %-6d duration: %dms\n name: %s\n", event.PID, event.DurationNS/1_000_000, name)
 	}
+
+	// use function name
 
 }
