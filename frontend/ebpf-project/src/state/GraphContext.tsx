bbee38664fe0ace1fc0f3f687151e0ecf6a2cb48
Paul Puma <archimago05@hotmail.com>
2026-08-27 03:47:04 -0700

    added context

---
+++ frontend/ebpf-project/src/state/GraphContext.tsx
@@ -0,0 +1,28 @@
+import{
+    createContext, use, useContext,useReducer, type ReactNode
+} from 'react'
+import {tracesReducer, initialTracesState, type TracesState, type GraphAction } from './graphReduce'
+
+type GraphContextValue = {
+    traces: TracesState
+    dispatch: React.Dispatch<GraphAction>
+}
+const GraphContext = createContext<GraphContextValue | null>(null)
+
+export function GraphProvider({children}: {children: ReactNode}){
+    const [traces, dispatch] = useReducer(tracesReducer, initialTracesState)
+
+    return(
+        <GraphContext.Provider value={{traces, dispatch}}>
+            {children}
+        </GraphContext.Provider>
+    )
+}
+
+export function useGraph(): GraphContextValue{
+    const context = useContext(GraphContext)
+    if (!context){
+        throw new Error("useGraph must be used within a GraphProvider")
+    }
+    return context
+}
